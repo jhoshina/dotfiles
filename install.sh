fc9f2eea0b805bbc15a51c62703450632b8ef9d2
Junichi Hoshina <jhoshina@gmail.com>
2023-06-27 21:05:50 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-ln -sf .zshrc ~/.zshrc
+SRC_DIR=`dirname $0`
+
+if [ ! -d "$HOME/.config" ]; then
+  mkdir "$HOME/.config"
+fi
+
+ln -sf $SRC_DIR/.zshrc $HOME/.zshrc
+ln -sf $SRC_DIR/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $SRC_DIR/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.confg
 
