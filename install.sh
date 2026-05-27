422472dc34cb3255ccdd0d1ba8e87a17e02b77bd
Junichi Hoshina <jhoshina@fout.jp>
2026-05-27 17:36:36 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-SRC_DIR=`dirname $0`
-
-if [ ! -d "$HOME/.config" ]; then
-  mkdir "$HOME/.config"
-fi
-
-ln -sf $SRC_DIR/.zshrc $HOME/.zshrc
-ln -sf $SRC_DIR/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $SRC_DIR/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.confg
+echo "underconstruction..."
 
